b9eaeaffeacdbb784d75ccd1db2d7d59175b8b8b
kerotori <javacode.svn@gmail.com>
2017-09-10 16:19:45 +0900

    プロジェクト ファイルを追加します。

---
+++ XML解析ツール/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XML解析ツール
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
